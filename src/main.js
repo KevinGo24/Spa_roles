5f1d43eb15dee9e82b44c31d6707c5b5d8b9bacd
kevin <kevin@gmail.com>
2026-06-03 11:47:47 -0500

    agregar cambios

--- src/main.js
+++ src/main.js
@@ -1,3 +1,25 @@
 import './style.css';
 import { enrutamiento } from "./router/router.js";
 import {  master_page} from "../src/controller/master.controller.js";
+
+// Dom de los campos del login
+
+// 1 - imput de roles
+const roles = document.getElementById("Cmb_role")
+// 2 - input de gmail
+const email = document.getElementById("Tbx_email")
+// 3 - input de pass
+const pass = document.getElementById("Tbx_password")
+// --------------------------------------------------
+
+// funcion de boton de login
+function login() {
+    // validacion de los campos 
+    if (roles.value == "" || email.value == "" || pass.value == "") {
+        alert('por favor llenar los campos')
+    }else{
+        window.location.hash = '#/dashboard';
+    }
+}
+
+
